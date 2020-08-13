c5dd0a879bf17b1efa8f3bf3cf695dc0e61f67e9
Yunus Emre Mert <yunusemremert@yandex.com>
2020-08-13 18:21:48 +0300

    CSS Variable, Navigation
    Navigation menü tasarımı ve css düzeltmeler.

--- components/button.js
+++ components/button.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import cn from 'classnames'
 
 import styles from './button.module.css'
 
-function Button({ children, label, ...props }) {
+function Button({ children, className, ...props }) {
   return (
-    <button type="button" className={styles.button} {...props}>
+    <button type="button" className={cn(styles.button, className)} {...props}>
       {children}
-      {label}
     </button>
   )
 }
