6492254704eace9fefcdc26ed12dd1d8e5fd631f
Yunus Emre Mert <yunusemremert@yandex.com>
2020-08-19 15:41:48 +0300

    Twitter API
    Twitter eklentisi ile api bağlantısı yapıldı.

---
+++ pages/api/tweet.js
@@ -0,0 +1,14 @@
+import Twit from '../../lib/twit'
+
+export default (req, res) => {
+  Twit.get('search/tweets', { q: '#Galatasaray', count: 10 }, function (
+    err,
+    data
+  ) {
+    if (err) {
+      return res.status(400).json({ message: 'Ups! bir hata oldu.' })
+    }
+
+    res.status(200).json(JSON.stringify(data))
+  })
+}
